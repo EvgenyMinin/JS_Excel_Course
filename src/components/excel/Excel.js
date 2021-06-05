184d37e066cbc3d8a0e07fa2315fe4f9e735fa59
EvgenyMinin <evgeny.frontend@gmail.com>
2021-06-05 20:31:55 +0300

    added structure for listeners

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -9,19 +9,21 @@ export class Excel {
   getRoot() {
     const $root = $.create('div', 'excel');
 
-    this.components.forEach(Component => {
+    this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className);
       const component = new Component($el);
       $el.html(component.toHTML());
       $root.append($el);
+
+      return component;
     });
 
     return $root;
   }
 
   render() {
-    const node = document.createElement('h1');
-    node.textContent = 'TEST';
     this.$el.append(this.getRoot().$el);
+
+    this.components.forEach(component => component.init());
   }
 }
